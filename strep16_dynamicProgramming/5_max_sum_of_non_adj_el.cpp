1af57f014c65eb3c0d624634a3e8cf0e256cd777
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2023-11-15 13:22:14 +0530

    did all 3 ways of solving robber house 1, now lets do robber house 2

--- strep16_dynamicProgramming/5_max_sum_of_non_adj_el.cpp
+++ strep16_dynamicProgramming/5_max_sum_of_non_adj_el.cpp
@@ -1,4 +1,6 @@
 // tabulation method: O(n) with constant space complexity
+
+// i submitted this nd it worked lool
 #include <bits/stdc++.h>
 int maximumNonAdjacentSum(vector<int> &ar) {
 	// Write your code here abhinay bir
@@ -14,3 +16,37 @@ int maximumNonAdjacentSum(vector<int> &ar) {
 	} return max(a, b);
 }
 
+// recursion approach: doing on my own, from left to right : WORKED
+int code(int i, int n, vector<int>ar) {
+	if (i >= n)return 0;
+
+	int pick = ar[i] + code(i + 2, n, ar);
+	int notPick = code(i + 1, n, ar);
+	return max(pick, notPick);
+
+
+}
+#include <bits/stdc++.h>
+int maximumNonAdjacentSum(vector<int> &ar) {
+
+
+	int n = ar.size();
+	return code(0, n, ar);
+}
+
+// memorization + recursion approach: doing on my own, from left to right : worked.
+int code(int i, int n, vector<int>ar, vector<int>&dp) {
+	if (i >= n)return 0;
+	if (dp[i] != -1)return dp[i];
+	int pick = ar[i] + code(i + 2, n, ar, dp);
+	int notPick = code(i + 1, n, ar, dp);
+	dp[i] =  max(pick, notPick); return dp[i];
+}
+#include <bits/stdc++.h>
+int maximumNonAdjacentSum(vector<int> &ar) {
+
+
+	int n = ar.size();
+	vector<int>dp(n, -1);
+	return code(0, n, ar, dp);
+}
